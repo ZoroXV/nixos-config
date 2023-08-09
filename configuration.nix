765293009c300fa738eba0844d5ac385bcc32e90
Victor LE CORRE <victor.le-corre@epita.fr>
2023-08-09 16:31:14 +0200

    refacto: Create hosts and base directory to improve readability

--- configuration.nix
+++ configuration.nix
@@ -61,14 +61,17 @@
   services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.zor0 = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  # };
+  users.mutableUsers = true;
+  users.users.zor0 = {
+    isNormalUser = true;
+    home = "/home/zor0";
+    shell = pkgs.bash;
+    uid = 4242;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
