ee9752e81d87fc2e11d269560f9051b59ba97bc7
Victor LE CORRE <victor.le-corre@epita.fr>
2023-08-10 02:00:58 +0200

    refacto(users): Move Users declaration from configuration.nix to base/users.nix

---
+++ base/users.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+{
+  users.mutableUsers = true;
+  users.users.zor0 = {
+    isNormalUser = true;
+    home = "/home/zor0";
+    shell = pkgs.bash;
+    uid = 4242;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
+  };
+}
