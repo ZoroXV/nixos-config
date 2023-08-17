0298b069501a7e80832002b870f53f75e9cdb624
Victor LE CORRE <victor.le-corre@epita.fr>
2023-08-18 00:35:53 +0200

    feat(zsh): Add ZSH configuration

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -1,4 +1,33 @@
-{ ... }:
-{
+{ pkgs, ... }:
+let
+  homeDirectory = "/home/zor0";
+  username = "zor0";
+  p10k = ./p10k.zsh;
+in {
   enable = true;
+  initExtra = ''
+    if [[ -r "${homeDirectory}/.cache/p10k-instant-prompt-${username}.zsh" ]]; then
+      source "${homeDirectory}/.cache/p10k-instant-prompt-${username}.zsh"
+    fi
+
+    # neofetch
+
+    [[ ! -f ${p10k} ]] || source ${p10k}
+  '';
+
+  plugins = [
+    {
+      name = "powerlevel10k";
+      src = pkgs.zsh-powerlevel10k;
+      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    }
+  ];
+
+  oh-my-zsh = {
+    enable = true;
+    plugins = [
+      "git"
+      "sudo"
+    ];
+  };
 }
