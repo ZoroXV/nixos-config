6b848892c86122ee8270b8c2098ab48f4b75964a
Victor LE CORRE <victor.le-corre@epita.fr>
2023-08-18 01:22:47 +0200

    feat(i3 + picom): Add basic i3 configuration + Enable picom to handle transparrency

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 {
-  #xsession.windowManager.i3.enable = true;
+  xsession.windowManager.i3 = import programs/i3 { inherit pkgs lib; };
 
   home = {
     stateVersion = "23.05";
@@ -9,10 +9,13 @@
     homeDirectory = "/home/zor0";
 
     packages = with pkgs; [
+      feh
       firefox
       neofetch
     ];
   };
 
   programs = import ./programs { inherit pkgs; };
+
+  services.picom.enable = true;
 }
