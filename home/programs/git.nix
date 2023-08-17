96028c52c7e6264bb315324b2ca7949e22793a8f
Victor LE CORRE <victor.le-corre@epita.fr>
2023-08-17 23:48:13 +0200

    feat(git): Add aliases + init.defaultBranch name

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -6,7 +6,22 @@
   userEmail = "victor.le-corre" + "@" + "epita.fr";
 
   aliases = {
+    c = "commit";
     s = "status";
+    co = "checkout";
+    cob = "checkout -b";
+    b = "branch";
+    f = "fetch";
+    pl = "pull";
+    ph = "push";
+    l = "log --oneline --graph";
+    ll = "log --graph";
+    ls = "log --stat";
+    lp = "log -p";
+    nb = "shortlog -sn";
+    rb = "rebase";
+    m = "merge";
+    r = "remote";
   };
 
   ignores = [ "*~" "*.swp" ".o" ".d" ];
@@ -14,6 +29,10 @@
   extraConfig = {
     commit = {
       verbose = true;
+    };
+
+    init = {
+      defaultBranch = "master";
     };
   };
 }
