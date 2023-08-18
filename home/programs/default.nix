9ffd6beb2be085d1ef2396f2d9790878a74490a0
Victor LE CORRE <victor.le-corre@epita.fr>
2023-08-18 12:18:10 +0200

    feat(rofi): Enable Rofi and Update i3 to replace dmenu by Rofi

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -4,9 +4,11 @@
   home-manager.enable = true;
   command-not-found.enable = true;
 
-  # Users Programs
-  git = import ./git.nix {};
-  zsh = import ./zsh { inherit pkgs; };
-
   alacritty = import ./alacritty.nix {};
+
+  git = import ./git.nix {};
+
+  rofi = import ./rofi { inherit pkgs; };
+
+  zsh = import ./zsh { inherit pkgs; };
 }
