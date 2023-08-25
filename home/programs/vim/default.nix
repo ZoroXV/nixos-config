f7bd6bbc9743d7737da55fc3691e60b4766747be
Victor LE CORRE <victor.le-corre@epita.fr>
2023-08-25 18:12:33 +0200

    feat(nvim): Replace Vim by Neovim

--- home/programs/vim/default.nix
+++ home/programs/vim/default.nix
@@ -1,21 +1,28 @@
 { pkgs, ... }:
 {
   enable = true;
+  vimAlias = true;
 
   plugins = with pkgs.vimPlugins; [
     lightline-vim
+
+    # Syntax Highlights
     rust-vim
     vim-nix
+    vim-go
+
+    # Color Schemes
     catppuccin-vim
+    nightfox-nvim
   ];
 
-  settings = {
-    number = true;
-    expandtab = true;
-    ignorecase = true;
-    shiftwidth = 4;
-    tabstop = 8;
-  };
+  # settings = {
+  #   number = true;
+  #   expandtab = true;
+  #   ignorecase = true;
+  #   shiftwidth = 4;
+  #   tabstop = 8;
+  # };
 
   extraConfig = builtins.readFile ./vimrc;
 }
